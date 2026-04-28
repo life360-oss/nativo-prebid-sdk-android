8841072b6e6286af469390d5f96b0e621373b3af
Matthew Murray <mattm.trinsic@gmail.com>
2026-04-28 12:32:53 -0700

    Change Nativo request epid back to 7 for now

--- PrebidMobile/PrebidMobile-core/src/main/java/com/nativo/prebidsdk/networking/NativoPathBuilder.kt
+++ PrebidMobile/PrebidMobile-core/src/main/java/com/nativo/prebidsdk/networking/NativoPathBuilder.kt
@@ -8,6 +8,6 @@ class NativoPathBuilder : URLPathBuilder() {
     }
 
     companion object {
-        const val NATIVO_ENDPOINT = "https://exchange.postrelease.com/esi.json?ntv_epid=54"
+        const val NATIVO_ENDPOINT = "https://exchange.postrelease.com/esi.json?ntv_epid=7"
     }
 }
